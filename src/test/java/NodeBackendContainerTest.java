2a3ab069ad74f2457154806867ee6bfdd6fa0daa
Alessio Coltellacci <lightplay8@gmail.com>
2018-09-26 00:23:52 +0200

    Remove unused imports

--- src/test/java/NodeBackendContainerTest.java
+++ src/test/java/NodeBackendContainerTest.java
@@ -4,11 +4,9 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URLConnection;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class NodeBackendContainerTest {
 
