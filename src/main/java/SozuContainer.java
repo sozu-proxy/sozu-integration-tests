f99adace1a628a6ed4c9b2aa084b1e8c7878b92c
Alessio Coltellacci <lightplay8@gmail.com>
2018-09-26 18:32:53 +0200

    Modify the wait strategy of sozu container
    The default wait strategy doesn't work with sozu and that throw an Exception

--- src/main/java/SozuContainer.java
+++ src/main/java/SozuContainer.java
@@ -1,16 +1,22 @@
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+import org.testcontainers.containers.wait.strategy.WaitStrategyTarget;
 import org.testcontainers.utility.MountableFile;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.Collections;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.net.HttpURLConnection;
+
 
 public class SozuContainer <SELF extends SozuContainer<SELF>> extends GenericContainer<SELF> {
 
-    private final static Logger LOGGER = Logger.getLogger(SozuContainer.class.getName());
+    private final static Logger log = Logger.getLogger(SozuContainer.class.getName());
 
     public static final String IMAGE = "clevercloud/sozu";
     public static final String DEFAULT_TAG = "latest";
@@ -23,6 +29,26 @@ public class SozuContainer <SELF extends SozuContainer<SELF>> extends GenericCon
 
     public SozuContainer(final String dockerImageName) {
         super(dockerImageName);
+
+        setWaitStrategy(new WaitStrategy() {
+            @Override
+            public void waitUntilReady(WaitStrategyTarget waitStrategyTarget) {
+                try {
+                    log.info("Trying to connect to " + getBaseUrl("http", 80));
+                    HttpURLConnection connection = (HttpURLConnection) getBaseUrl("http", 80).openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+                    log.info("Get response code " + connection.getResponseCode());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public WaitStrategy withStartupTimeout(Duration startupTimeout) {
+                return null;
+            }
+        });
     }
 
     @Override
