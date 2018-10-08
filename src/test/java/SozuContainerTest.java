570516789f354e40bee26331c7b4d9b440a0cbe5
Alessio Coltellacci <lightplay8@gmail.com>
2018-10-08 14:05:39 +0200

    Add test: path begin

--- src/test/java/SozuContainerTest.java
+++ src/test/java/SozuContainerTest.java
@@ -144,4 +144,20 @@ public class SozuContainerTest {
 
         assertEquals(HTTP_OK, res.getStatusLine().getStatusCode());
     }
+
+    @Test
+    public void testPathbegin() throws Exception {
+        URL sozuUrl = sozuContainer.getBaseUrl("http", SozuContainer.DEFAULT_HTTP_PORT);
+
+        // see config.toml
+        String hostname = "pathbegin.com";
+        String pathPrefix = "/api";
+        String url = String.format("-H 'Host: %s' %s", hostname, sozuUrl.toString());
+
+        HttpResponse res = curl(url);
+        HttpResponse resWithPathBegin = curl(url + pathPrefix);
+
+        assertEquals(HTTP_UNAVAILABLE, res.getStatusLine().getStatusCode());
+        assertEquals(HTTP_OK, resWithPathBegin.getStatusLine().getStatusCode());
+    }
 }
